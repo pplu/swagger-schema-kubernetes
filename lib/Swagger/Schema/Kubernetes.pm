8b34b5ddfd413e4cfea2dfabb04b7c40d77b9d6c
Jose Luis Martinez <jlmartinez@capside.com>
2019-01-02 11:14:34 +0000

    Documentation

--- lib/Swagger/Schema/Kubernetes.pm
+++ lib/Swagger/Schema/Kubernetes.pm
@@ -45,3 +45,63 @@ package Swagger::Schema::Kubernetes;
   object definitions => (isa => 'Swagger::Schema::KubernetesSchema');
 
 1;
+### main pod documentation begin ###
+ 
+=encoding UTF-8
+ 
+=head1 NAME
+ 
+Swagger::Schema::Kubernetes - Object model to Kubernetes Swagger / OpenAPI schema files
+ 
+=head1 SYNOPSIS
+ 
+  use File::Slurp;
+  my $data = read_file($swagger_file);
+  my $schema = Swagger::Schema::Kubernetes->MooseX::DataModel::new_from_json($data);
+  # use the object model
+  say "This API consists of:";
+  foreach my $path (sort keys %{ $schema->paths }){
+    foreach my $http_verb (sort keys %{ $schema->paths->{ $path } }) {
+      say "$http_verb on $path";
+    }
+  }
+ 
+=head1 DESCRIPTION
+ 
+Get programmatic access to Kubenertes Swagger / OpenAPI files.
+
+This module builds upon L<Swagger::Schema> to produce the same object model, enabling
+access to the extra properties that the Kubernetes swagger definitions adds.
+ 
+=head1 OBJECT MODEL
+ 
+The object model is defined with L<MooseX::DataModel>. Take a look at the
+C<lib/Swagger/Schema/Kubernetes.pm> and C<lib/Swagger/Schema.pm> files or the swagger spec 
+to know what you can find inside the objects
+ 
+=head1 SEE ALSO
+
+L<https://github.com/kubernetes/kubernetes/tree/master/api/openapi-spec>
+ 
+L<https://github.com/OAI/OpenAPI-Specification>
+ 
+L<http://swagger.io>
+ 
+=head1 AUTHOR
+ 
+    Jose Luis Martinez
+    CAPSiDE
+    jlmartinez@capside.com
+ 
+=head1 BUGS and SOURCE
+ 
+The source code is located here: https://github.com/pplu/swagger-schema-kubernetes
+ 
+Please report bugs to: https://github.com/pplu/swagger-schema-kubernetes/issues
+ 
+=head1 COPYRIGHT and LICENSE
+ 
+Copyright (c) 2018 by CAPSiDE
+ 
+This code is distributed under the Apache 2 License. The full text of the 
+license can be found in the LICENSE file included with this module.
